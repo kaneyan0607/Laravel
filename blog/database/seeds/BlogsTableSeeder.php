0e04cbfcf1808b6081699ef864d84d69c7e5c231
kaneyan0607 <kaneyan0607@gmail.com>
2021-04-01 18:26:39 +0900

    add:fakerでテストデータ

---
+++ blog/database/seeds/BlogsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Blog;
+use Carbon\Factory;
+
+class BlogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Blog::class, 15)->create();
+    }
+}
