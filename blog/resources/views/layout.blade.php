2550e6d441fca8bf25e8d2d23bee67915e15f88c
kaneyan0607 <kaneyan0607@gmail.com>
2021-04-08 16:59:14 +0900

    add:ブログ編集機能まで →編集対象のidは渡せるが更新できない

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    <!-- <link rel="stylesheet" href="../../public/css/app.css"> -->
-    <link rel="stylesheet" href="../../../../Laravel/blog/public/css/app.css">
-    <script src="../../../../Laravel/blog/public/js/app.js" defer></script>
+    <link rel="stylesheet" href="css/app.css">
+    <!-- <link rel="stylesheet" href="../../../../Laravel/blog/public/css/app.css"> -->
+    <!-- <script src="../../../../Laravel/blog/public/js/app.js" defer></script> -->
+    <script src="js/app.js" defer></script>
 </head>
 
 <body>
