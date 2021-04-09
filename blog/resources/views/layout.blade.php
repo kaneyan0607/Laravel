c8b1fbb3b433d11db8be63f45d42004af9891099
kaneyan0607 <kaneyan0607@gmail.com>
2021-04-09 16:27:25 +0900

    fix:共通レイアウト相対パスを修正

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="css/app.css">
-    <!-- <link rel="stylesheet" href="../../../../Laravel/blog/public/css/app.css"> -->
-    <!-- <script src="../../../../Laravel/blog/public/js/app.js" defer></script> -->
-    <script src="js/app.js" defer></script>
+    <link rel="stylesheet" href="/Laravel/blog/public/css/app.css">
+    <script src="/Laravel/blog/public/css/app.css" defer></script>
 </head>
 
 <body>
