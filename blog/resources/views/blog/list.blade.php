ca0ea64708d3cb6532b91dcc259e381f59ca1a96
kaneyan0607 <kaneyan0607@gmail.com>
2021-04-09 16:11:38 +0900

    add:データ削除のメソッド

--- blog/resources/views/blog/list.blade.php
+++ blog/resources/views/blog/list.blade.php
@@ -8,7 +8,7 @@
     4.(V)詳細画面を作る
 -->
 <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-10 col-md-offset-2">
         <h2>ブログ記事一覧</h2>
         @if (session('err_msg'))
         <p class="text-danger">{{ session('err_msg') }}</p>
@@ -19,6 +19,7 @@
                 <th>タイトル</th>
                 <th>日付</th>
                 <th></th>
+                <th></th>
             </tr>
             @foreach($blogs as $blog)
             <tr>
@@ -26,10 +27,23 @@
                 <td><a href="blog/{{ $blog->id }}">{{ $blog->title }}</a></td>
                 <td>{{ $blog->updated_at }}</td>
                 <td><button type="button" class="btn btn-primary" onclick="location.href='blog/edit/{{ $blog->id }}'">編集</button></td>
+
+                <form method="POST" action="{{ route('delete', $blog->id) }}" onSubmit="return checkDelete()">
+                    @csrf
+                    <td><button type="submit" class="btn btn-primary" onclick=>削除</button></td>
+                </form>
             </tr>
             @endforeach
         </table>
     </div>
 </div>
-
+<script>
+    function checkDelete() {
+        if (window.confirm('削除してよろしいですか？')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+</script>
 @endsection
