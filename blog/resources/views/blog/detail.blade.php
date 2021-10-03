abdaf84247f84195f2b617e307966796aad08bb1
kaneyan0607 <kaneyan0607@gmail.com>
2021-10-03 22:21:23 +0900

    fix:データが無い時のリダイレクト処理

--- blog/resources/views/blog/detail.blade.php
+++ blog/resources/views/blog/detail.blade.php
@@ -4,11 +4,6 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <h2>{{$blog->title}}</h2>
-        @if(session('err_msg'))
-        <p class="text-danger">
-            {{session('err_msg')}}
-        </p>
-        @endif
         <span>作成日 :{{$blog->created_at}}</span>
         <span>更新日 :{{$blog->updated_at}}</span>
         <p>{{$blog->content}}</p>
