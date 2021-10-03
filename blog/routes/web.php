8a57f16a22578e02e54fcefd14fdffc4ae46fed2
kaneyan0607 <kaneyan0607@gmail.com>
2021-10-03 22:46:31 +0900

    add:ブログ投稿画面

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,9 +14,11 @@
 //ブログ一覧画面を表示
 Route::get('/', 'BlogController@showList')->name('blogs');
 
+//ブログ登録画面を表示
+Route::get('/blog/create', 'BlogController@showCreate')->name('create');
+
+//ブログ登録画面
+Route::get('/blog/store', 'BlogController@exeStore')->name('store');
+
 //ブログ詳細画面を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
-
-// Route::get('/welcome', function () {
-//     return view('welcome');
-// });
